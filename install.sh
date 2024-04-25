209c49413ded3ade37b856e444cb6b6ddafc7578
Jesus Chinchillas <jesus.chinchillas@megacable.com.mx>
2024-04-25 11:29:05 -0600

    new dotdiles

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "RUNNING dotfiles repo install.sh"
+
+# echo "STEP 1: 💾 copying .gitconfig and .gitignore_global"
+# cp -r ./git/.gitconfig ./git/.gitignore_global ~
+
+echo "🐚 Shell is $SHELL"
+echo "STEP 2: 💾 copying shell configuration files e.g., bash, zsh"
+
+if [ "$SHELL" == "/bin/bash" ]; then 
+  cp ./shell/bash/.bashrc $HOME/.bashrc
+  cp ./shell/bash/.bash_profile $HOME/.bash_profile  
+elif [ "$SHELL" == "/bin/zsh" ]; then
+  cp ./shell/zsh/.zshrc $HOME/.zshrc
+else
+  echo "no unix shell dotfiles copied"
+fi
+
+echo "STEP 3: 💾 copying VS Code-related config files e.g., settings, keybindings, tasks"
---
+++ shell/bash/.bashrc
@@ -0,0 +1,22 @@
+
+export HISTFILE="$HOME/.bash_history"
+export HISTSIZE=10000
+export HISTFILESIZE=100000
+export HISTCONTROL=ignoredups:erasedups
+# append to the history file instead of overwriting it when shell closed
+shopt -s histappend
+
+
+# colors
+red='\e[0;31m'
+RED='\e[1;31m'
+blue='\e[0;34m'
+BLUE='\e[1;34m'
+cyan='\e[0;36m'
+CYAN='\e[1;36m'
+NC='\e[0m'
+
+# https://ss64.com/bash/syntax-prompt.html
+# https://www.gnu.org/software/bash/manual/html_node/Controlling-the-Prompt.html
+unset PROMPT_COMMAND
+export PS1="🦈  \W \$ "
